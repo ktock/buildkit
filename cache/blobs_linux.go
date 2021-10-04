7f95ecc9d05a0e51b68613d5172fd07d0bd68cf3
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2021-10-04 10:24:06 +0900

    tmp

--- cache/blobs_linux.go
+++ cache/blobs_linux.go
@@ -72,7 +72,7 @@ func (sr *immutableRef) tryComputeOverlayBlob(ctx context.Context, lower, upper 
 		}
 	}()
 
-	bufW := bufio.NewWriterSize(cw, 65536)
+	bufW := bufio.NewWriterSize(cw, 32768)
 	var labels map[string]string
 	if compressorFunc != nil {
 		dgstr := digest.SHA256.Digester()
